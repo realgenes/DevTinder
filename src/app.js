4207e80538756ebdef5fc307da7ef9e45af6dcb5
Shishupal <its.shishupal@gmail.com>
2025-04-14 10:31:05 +0530

    exploring get, post , delete

--- src/app.js
+++ src/app.js
@@ -1,18 +1,28 @@
-const express = require('express');
-
+const express = require("express");
 
 const app = express();
 
 
-app.use("/hello",(req, res) => {
-  res.send("Hello from DevTinder Team!");
+app.get("/user", (req, res) => {
+  res.send({firstName:"Devil", lastName:"World"})
+})
+
+app.post("/user", (req, res) => {
+  res.send("Data saved successfully!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data deleted successfully!");
 });
 
 app.use("/test", (req, res) => {
-  res.send("Tester from DevTinder!");
+  res.send("Tester from Devil!");
 });
+
+
+
+
 
 app.listen(3000, () => {
   console.log("server is successfully running on port 3000");
-  
 });
