6aa0d372a4ffc89c18e84dd4f404c594572b09a5
Shishupal <its.shishupal@gmail.com>
2025-06-13 21:07:05 +0530

    feed API, user API

--- src/app.js
+++ src/app.js
@@ -17,6 +17,49 @@ app.post("/signup", async (req, res) => {
 
 })
 
+//get user data using email
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+
+    if (!user) {
+      res.send('user not found')
+    } else {
+       res.send(user);
+    }
+
+   
+
+
+    // const user = await User.find({ emailId: userEmail });
+
+    // if (user.length === 0) {
+    //   res.status(401).send('user not found');
+    // } else {
+    //    res.send(user);
+    // }
+   
+    
+  } catch (error) {
+    res.status(500).send("some error happened");
+  }
+});
+
+//feed API - GET /feed - get all the users from the database
+app.get("/feed",async(req,res) => {
+  
+  try {
+    const users = await User.find({});
+    res.send(users);
+    
+  } catch (error) {
+    res.status(401).send('something went  wrong')
+  }
+})
+
 
 
 
