badeba77725e9a569393fffc5ce065afa30b2600
Shishupal <its.shishupal@gmail.com>
2025-07-02 22:22:38 +0530

    created Routes for Profile, Auth , Request

--- src/app.js
+++ src/app.js
@@ -1,113 +1,17 @@
 const express = require("express");
 const connectDB = require("./config/database");
-const { userAuth} = require('./middlewares/auth');
 const app = express();
-const User = require('./models/user');
-const bcrypt = require('bcrypt');
 const cookieParser = require("cookie-parser")
-const jwt = require("jsonwebtoken");
-const{validateSignUpData} = require('./utils/validation')
+
 
 app.use(express.json());
 app.use(cookieParser());
 
-//sign up api
-app.post("/signup", async (req, res) => {
-  try {
-    //validate data
-    validateSignUpData(req);
+const authRouter = require('./routers/auth');
+const profileRouter = require('./routers/profile')
+const requestRouter = require('./routers/request');
 
-    const { password, firstName, lastName, emailId, skills, age, gender } = req.body;
-
-    //encrypting password
-    const passwordHash = await bcrypt.hash(password, 10);
-
-    //creating new instance
-    const user = new User({
-      firstName,
-      lastName,
-      skills,
-      gender,
-      age,
-      emailId,
-      password: passwordHash,
-    });
-
-    await user.save();
-    res.send("user added successfully!");
-  } catch (err) {
-    res.status(400).send('Error :' + err.message);
-  }
-
-});
-
-//login api
-app.post("/login", async (req, res) => {
-  try {
-    const { emailId, password } = req.body;
-
-    if (!emailId || !password) {
-      throw new Error('emailId and password are required!');
-    }
-
-    const user = await User.findOne({ emailId: emailId });
-
-    if (!user) {
-      throw new Error('Invalid credintials!')
-    }
-
-    if (!user.password) {
-      throw new Error('User password not set in DB');
-    }
-
-    const isPasswordValid = await user.validatePassword(password);
-
-    if (isPasswordValid) {
-      //create a jwt token
-      const token = await user.getJWT();
-      //add token to cookie
-      res.cookie("token", token, {
-        expires: new Date(Date.now() + 1* 3600000),
-      });
-
-      res.send('login successful !');
-    } else {
-      throw new Error('password is not valid !')
-    }
-    
-  } catch (error) {
-    res.status(400).send("ERROR: " + error.message);
-  }
-});
-
-//profile api
-app.get("/profile", userAuth,async (req, res) => {
-  try {
-     
-
-    const user = req.user;
-    
-    res.send(user);
-    
-  } catch (error) {
-    res.status(400).send("ERROR: " + error.message);
-  }
-
- 
-})
-
-
-//send connection request
-app.post("/sendConnection", userAuth, (req, res) => {
-  
-  const user = req.user;
-
-  
-  res.send(user.firstName +' sent connection request !');
-})
-
-
-
+app.use("/", authRouter); 
 
 
 
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+const {validateSignUpData} = require('../utils/validation')
 const authRouter = express.Router();
 
 authRouter.post("/signup", async (req, res) => {
@@ -27,6 +30,44 @@ authRouter.post("/signup", async (req, res) => {
     res.send("user added successfully!");
   } catch (err) {
     res.status(400).send("Error :" + err.message);
+  }
+});
+
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    if (!emailId || !password) {
+      throw new Error('emailId and password are required!');
+    }
+
+    const user = await User.findOne({ emailId: emailId });
+
+    if (!user) {
+      throw new Error('Invalid credintials!')
+    }
+
+    if (!user.password) {
+      throw new Error('User password not set in DB');
+    }
+
+    const isPasswordValid = await user.validatePassword(password);
+
+    if (isPasswordValid) {
+      //create a jwt token
+      const token = await user.getJWT();
+      //add token to cookie
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 1* 3600000),
+      });
+
+      res.send('login successful !');
+    } else {
+      throw new Error('password is not valid !')
+    }
+    
+  } catch (error) {
+    res.status(400).send("ERROR: " + error.message);
   }
 });
 
